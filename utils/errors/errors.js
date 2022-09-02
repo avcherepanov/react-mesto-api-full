cc43467a4531f0026c5b4c5dbfc5ec2a598a4141
Artem Cherepanov <nf11082016@gmail.com>
2022-09-02 16:00:47 +0500

    02.09.2022

--- utils/errors/errors.js
+++
@@ -1,9 +0,0 @@
-const BAD_REQUEST = 400;
-const NOT_FOUND = 404;
-const ERROR_CREATED = 201;
-
-module.exports = {
-  BAD_REQUEST,
-  NOT_FOUND,
-  ERROR_CREATED,
-};
--- utils/validateURL/validateURL.js
+++
@@ -1,8 +0,0 @@
-const validateURL = (URL) => {
-  if (URL !== URL.match(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/).join('')) {
-    throw new Error('Неверный формат ссылки');
-  }
-  return URL;
-};
-
-module.exports = validateURL;
