a6f959fa1997008e5dd01baa168673b06754cbad
Artem Cherepanov <nf11082016@gmail.com>
2022-09-04 18:00:11 +0500

    04.09.2022

--- backend/app.js
+++ backend/app.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
 
